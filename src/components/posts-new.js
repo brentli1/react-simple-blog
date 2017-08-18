15d6f61c6db73c1af877cd5cc9c7ac16aa7d4f48
brentli1 <brent.jlinville@gmail.com>
2017-08-18 16:42:22 -0500

    add initial form controls using redux-form for new posts

--- src/components/posts-new.js
+++ src/components/posts-new.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
+  renderTitleField(field) {
+    return (
+      <input
+        {...field.input}
+        className="form-control"
+        type="text"
+      />
+    );
+  }
+
   render() {
     return (
-      <div>New Post</div>
+      <form>
+        <label htmlFor="title" className="control-lab">Blog Title</label>
+        <Field 
+          name="title"
+          component={this.renderTitleField}
+          className="form-control"
+        />
+      </form>
     );
   }
 }
 
-export default PostsNew;
+export default reduxForm({
+  form: 'PostsNewForm'
+})(PostsNew);
